ac0dcd72bb00ce3e210b4bb247652b1b5c5652fd
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-15 11:04:39 +0100

    ref: Update mobile menu snippets and add shadcn drawer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
 import MainMenu from "@/components/main-menu";
-import Image from "next/image";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <MainMenu />
+        <main className="flex min-h-screen flex-col items-center justify-center p-16 max-w-xl mx-auto gap-y-4">
+            <div>
+                <h1 className="font-bold text-2xl mb-2">Mobile Menu Snippets</h1>
+                <p>
+                    This project showcases various mobile menu snippets built using the shadcn drawer component. The
+                    drawer component can be configured to slide out from different directions, providing a versatile
+                    solution for mobile navigation. The table below lists the different menu directions along with a
+                    corresponding icon. Click on an icon to see the menu in action.
+                </p>
+            </div>
+            <Table>
+                <TableCaption>
+                    Mobile menus build using{" "}
+                    <a href="https://ui.shadcn.com/docs/components/drawer" className="underline">
+                        shadcn drawer component
+                    </a>
+                </TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px] font-bold">Direction</TableHead>
+                        <TableHead></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">Right</TableCell>
+                        <TableCell>
+                            <MainMenu direction="right" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">Left</TableCell>
+                        <TableCell>
+                            <MainMenu direction="left" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">Top</TableCell>
+                        <TableCell>
+                            <MainMenu direction="top" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">Bottom</TableCell>
+                        <TableCell>
+                            <MainMenu direction="bottom" />
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
         </main>
     );
 }
