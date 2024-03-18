8ad71ab870a2ee30227369365443d0733be92555
Maciej Adamski <maciej.adamski@kyndryl.com>
2024-03-18 19:50:03 +0100

    feat: add close icon

---
+++ src/components/close-icon.tsx
@@ -0,0 +1,20 @@
+import IconClickableArea from "./icon-clickable-area";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+// You can customize the icon by replacing the <Bars3Icon /> component with your own SVG-JSX icon.
+function CustomIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
+            <path fill="black" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z"></path>
+        </svg>
+    );
+}
+
+export default function CloseIcon() {
+    return (
+        <IconClickableArea>
+            <XMarkIcon className="w-6 h-6 text-black" />
+            {/* <CustomIcon /> */}
+        </IconClickableArea>
+    );
+}
